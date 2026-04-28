006b73638fc5cf955c6968cedef43f94f1f88448
ravitejashivanadhuni <ravitejashivanadhuni@gmail.com>
2026-04-28 22:19:50 +0530

    made changes in exams page

--- src/components/Exam_page_components/Exam_card.jsx
+++ src/components/Exam_page_components/Exam_card.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import API_BASE_URL from "../../config";
+import API_BASE_URL from "../../config/api";
 
 const S = {
   white: "#ffffff",
